d15e237a81052d35e40bdfc8615b1a37a2ddb2e6
thanhuit23 <thanhtv@uit.edu.vn>
2024-03-02 09:01:03 +0000

    from localhost -> 157.230.41.69

--- src/react-components/room/EnterOnDeviceModal.stories.js
+++ src/react-components/room/EnterOnDeviceModal.stories.js
@@ -9,14 +9,14 @@ export default {
   }
 };
 
-export const Base = () => <RoomLayout viewport={<EnterOnDeviceModal shortUrl="hub.link" code="IDEB" />} />;
+export const Base = () => <RoomLayout viewport={<EnterOnDeviceModal shortUrl="157.230.41.69" code="IDEB" />} />;
 
 export const HeadsetConnected = () => (
-  <RoomLayout viewport={<EnterOnDeviceModal shortUrl="hub.link" code="IDEB" headsetConnected />} />
+  <RoomLayout viewport={<EnterOnDeviceModal shortUrl="157.230.41.69" code="IDEB" headsetConnected />} />
 );
 
 export const VrNotSupported = () => (
-  <RoomLayout viewport={<EnterOnDeviceModal unsupportedBrowser shortUrl="hub.link" code="IDEB" headsetConnected />} />
+  <RoomLayout viewport={<EnterOnDeviceModal unsupportedBrowser shortUrl="157.230.41.69" code="IDEB" headsetConnected />} />
 );
 
 export const LoadingCode = () => <RoomLayout viewport={<EnterOnDeviceModal loadingCode />} />;
