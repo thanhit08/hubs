e90987b9e579dd25039d7babe4d5f9db95222a80
thanhuit23 <thanhtv@uit.edu.vn>
2024-04-09 17:05:08 +0000

    Add loading effect

--- src/components/page-thumbnail.js
+++ src/components/page-thumbnail.js
@@ -1,5 +1,10 @@
 import { resolveUrl, createImageTexture } from "../utils/media-utils";
 import { proxiedUrlFor } from "../utils/media-url-utils";
+import { waitForDOMContentLoaded } from "../utils/async-utils";
+import loadingObjectSrc from "../assets/models/LoadingObject_Atom.glb";
+import { cloneObject3D } from "../utils/three-utils";
+
+let loadingObject;
 
 AFRAME.registerComponent("page-thumbnail", {
     schema: {
@@ -9,6 +14,11 @@ AFRAME.registerComponent("page-thumbnail", {
     init: function () {
         console.log("Init Page");
         this.updateThumbnail = this.updateThumbnail.bind(this);
+        waitForDOMContentLoaded().then(() => {
+            loadModel(loadingObjectSrc).then(gltf => {
+                loadingObject = gltf;
+            });
+        });
     },
 
     update(prevData) {
