d6f4518200df5ce187a9d72eb5c1eba8187e0d8f
thanhuit23 <thanhtv@uit.edu.vn>
2024-04-19 02:25:19 +0000

    Change Exit Button

--- src/react-components/room/WebGLContentModal.js
+++ src/react-components/room/WebGLContentModal.js
@@ -10,6 +10,10 @@ import { Button } from "../input/Button";
 import { FormattedMessage } from "react-intl";
 import { Column } from "../layout/Column";
 import styles from "../../assets/stylesheets/preferences-screen.scss";
+import { ToolbarButton } from "../input/ToolbarButton";
+import { ReactComponent as LeaveIcon } from "../icons/Leave.svg";
+
+
 
 export function WebGLContentModal({ onClose }) {
     const { isSubmitting, handleSubmit, register, errors } = useForm({
@@ -20,13 +24,18 @@ export function WebGLContentModal({ onClose }) {
 
     return (
         <div className={classNames(styles.preferencesPanel)} style={{ position: 'relative' }}>
-            <CloseButton onClick={onClose} style={{ position: 'absolute', top: 0, left: 0 }} />
+            <ToolbarButton
+                icon={<LeaveIcon />}
+                preset={"accent2"}
+                onClick={onClose}
+                style={{ position: 'absolute', top: 12, left: 24 }}
+            />
             <iframe
                 id="inlineFrameExample"
                 title="반응과 반사의 구조"
                 width="100%"
                 height="100%"
-                src="https://webglmulti.web.app/M1" scrolling="no" style={{ frameBorder: 0 }} frameborder="0" allow="xr-spatial-tracking">
+                src="https://webglmulti.web.app/M1" scrolling="no" style={{ frameBorder: 0 }} frameBorder="0" allow="xr-spatial-tracking">
             </iframe>
 
         </div>
