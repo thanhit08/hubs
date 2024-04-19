235d3f235cd0108bfeb751d1a8cd2f0f0225cfe1
thanhuit23 <thanhtv@uit.edu.vn>
2024-04-19 01:03:06 +0000

    View full screen webgl content

--- src/react-components/room/WebGLContentModal.js
+++ src/react-components/room/WebGLContentModal.js
@@ -4,10 +4,12 @@ import { Modal } from "../modal/Modal";
 import { CloseButton } from "../input/CloseButton";
 import { TextInputField } from "../input/TextInputField";
 import { useForm } from "react-hook-form";
+import classNames from "classnames";
 import { useState } from "react";
 import { Button } from "../input/Button";
 import { FormattedMessage } from "react-intl";
 import { Column } from "../layout/Column";
+import styles from "../../assets/stylesheets/preferences-screen.scss";
 
 export function WebGLContentModal({ onClose }) {
     const { isSubmitting, handleSubmit, register, errors } = useForm({
@@ -17,18 +19,17 @@ export function WebGLContentModal({ onClose }) {
     });
 
     return (
-        <Modal
-            title={<FormattedMessage id="webgl-content-modal.title" defaultMessage="반응과 반사의 구조" />}
-            beforeTitle={<CloseButton onClick={onClose} width="100%" />}>
+        <div className={classNames(styles.preferencesPanel)} style={{ position: 'relative' }}>
+            <CloseButton onClick={onClose} style={{ position: 'absolute', top: 0, left: 0 }} />
             <iframe
                 id="inlineFrameExample"
                 title="반응과 반사의 구조"
                 width="100%"
-                height="700"
-                src="https://webglmulti.web.app/M1" scrolling="no" style={{ border: 0}} frameborder="0" allow="xr-spatial-tracking">
+                height="100%"
+                src="https://webglmulti.web.app/M1" scrolling="no" style={{ frameBorder: 0 }} frameborder="0" allow="xr-spatial-tracking">
             </iframe>
 
-        </Modal>
+        </div>
     );
 }
 
